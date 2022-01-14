ef7ab78523a78120113ec9d0d9fd0a59e8df2e3f
Andrew Kim <andrewkim@Andrews-MacBook-Pro-2.local>
2022-01-13 21:22:22 -0600

    first draft

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,39 +1,89 @@
+# import os
+# import csv
+
+# csvpath = os.path.join('Resource', 'budget_data.csv')
+# sum = 0
+# sumchg = 0
+
+# with open(csvpath) as csvfile:
+#     csvreader = csv.reader(csvfile, delimiter=",")
+
+#     # Skip the header
+#     next(csvreader)
+#     first_row=next(csvreader)
+#     value = int(first_row[1])
+#     # Pass the raw text data as a list
+#     csvdata = list(csvreader)
+#     for row in csvdata:
+#         sum = sum + int(row[1])
+#         print(sum)
+#         change = int(row[1]) - value
+#         sumchg = sumchg + change
+#         value = int(row[1]) 
+    
+
+# total_months = len(list(csvdata))
+  
+#     # SRead each row of data after the header
+
+# print(sumchg)
+# Avgchg = sumchg/total_months
+# print("Financial Analysis")
+# print("---------------------")
+# print(f"Total Months: {total_months}")
+# print(f"Total: {sum}")
+# print(f"Average Change: {Avgchg}")
+
 import os
 import csv
 
 csvpath = os.path.join('Resource', 'budget_data.csv')
-
+sum = 0
+sumchg = 0
+months=1
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     # Skip the header
     next(csvreader)
-
+    first_row=next(csvreader)
+    value = int(first_row[1])
+    print(value)
+    sum  =int(first_row[1])
+    print(sum)
     # Pass the raw text data as a list
     csvdata = list(csvreader)
+    for row in csvdata:
+        months = months + 1
+        sum = sum + int(row[1])
+        change = int(row[1]) - value
+        sumchg = sumchg + change
+        value = int(row[1]) 
+    
+total_months = len(list(csvdata))+1
 
-total_months = len(list(csvdata))
   
     # SRead each row of data after the header
-sum = 0
-for row in csvdata:
-    sum = sum + int(row[1])
-    Avg = int(row[1]) - sum 
-    print(Avg)
-Avgchg = Avg/total_months
-print(Avgchg)
+
+print(sumchg)
+Avgchg = sumchg/total_months
 print("Financial Analysis")
 print("---------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: {sum}")
+print(f"Average Change: {Avgchg}")
 
-   
 
-   # Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $38382578 
-# Average  Change: $-2315.12
-# Greatest Increase in Profits: Feb-2012 ($1926159)
-# Greatest Decrease in Profits: Sep-2013 ($-2196167)
+with open(csvpath) as csvfile:
 
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Skip over the csv file header on the first row
+    next(csvreader)
+
+    # Pass the raw text data out as a list
+    csv_data = list(csvreader)
+    
+  
+for row in csv_data:
+    print(f"{row[0]:8}  {int(row[1]):14,.0f}")
