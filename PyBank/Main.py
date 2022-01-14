e5c4c2a6ad56402eadf5888aff81800569946b06
Andrew Kim <andrewkim@Andrews-MacBook-Pro-2.local>
2022-01-13 19:15:16 -0600

    adding test

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -4,12 +4,36 @@ import csv
 csvpath = os.path.join('Resource', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
+    # Skip the header
+    next(csvreader)
 
-    #print(csvreader)
+    # Pass the raw text data as a list
+    csvdata = list(csvreader)
 
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+total_months = len(list(csvdata))
+  
+    # SRead each row of data after the header
+sum = 0
+for row in csvdata:
+    sum = sum + int(row[1])
+    Avg = int(row[1]) - sum 
+    print(Avg)
+Avgchg = Avg/total_months
+print(Avgchg)
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: {sum}")
+
+   
+
+   # Financial Analysis
+# ----------------------------
+# Total Months: 86
+# Total: $38382578 
+# Average  Change: $-2315.12
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($-2196167)
+
